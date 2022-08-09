fef4aedbb879c75cf3cdfa0039ba5956922f40e8
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-08-09 18:02:35 +0300

    add network manager unit test

--- EnparaCaseTests/EnparaCaseTests.swift
+++ EnparaCaseTests/EnparaCaseTests.swift
@@ -11,14 +11,34 @@ import XCTest
 class EnparaCaseTests: XCTestCase {
 
     override func setUpWithError() throws {
-        
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
+    func testGetRequest() throws{
+        let expectation = XCTestExpectation(description: #function)
+        let testPath = "https://api.themoviedb.org/3/movie/top_rated?api_key=11459cff1c1ce00e3202addab99f3a91&language=en-us&page=1"
+        var resultData: Data?
+        var resultCode: Int?
         
+        NetworkManager<MockModel>.apiRequest(testPath, method: .get, parameters: nil) { response in
+            switch response {
+                
+            case .success(let responseData):
+                resultData = responseData.data
+                resultCode = responseData.code
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("The get process was be failed by:\(error.localizedDescription)")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 15.0)
+        XCTAssert(resultData != nil, "Test Passed")
+        XCTAssert(resultCode != nil, "Test Passed")
+        XCTAssert(resultCode == 200, "Test Passed")
     }
 }
